3efddfad2e41fad3e4930e3a2cb7d7879f0266ad
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-09-09 01:02:03 +0200

    test(🟢): negative number

--- src/test/divisionByTen.test.ts
+++ src/test/divisionByTen.test.ts
@@ -1,4 +1,5 @@
 function divisionByTen(number: number) {
+    number = Math.abs(number)
     if (number >= 1) {
         const resultOfDivision: number = number / 10
         return divisionByTen(resultOfDivision) + 1
